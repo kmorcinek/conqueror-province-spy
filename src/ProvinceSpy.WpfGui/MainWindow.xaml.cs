dfc0fd4e3ad0c89d9c255e2f6663903853641ac9
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2012-12-28 21:00:46 +0100

    Button clicks moved from code-behind to ICommand

--- src/ProvinceSpy.WpfGui/MainWindow.xaml.cs
+++ src/ProvinceSpy.WpfGui/MainWindow.xaml.cs
@@ -9,98 +9,11 @@ namespace ProvinceSpy.WpfGui
     public partial class MainWindow : Window
     {
         private readonly MainWindowsViewModel viewModel = new MainWindowsViewModel();
-        private readonly List<ProvinceHistory> provinceHistories = new List<ProvinceHistory>();
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = viewModel;
-        }
-
-        private void StartButtonClick(object sender, RoutedEventArgs e)
-        {
-            SetUp();
-            var button = sender as Button;
-
-            if (button != null)
-            {
-                button.Visibility = Visibility.Hidden;
-            }
-        }
-
-        private void SetUp()
-        {
-            viewModel.DatabaseObjects.Clear();
-            var capital = cmbCapital.SelectedValue;
-            if (capital != null)
-            {
-                var predictor = new Predictor();
-                foreach (var neighbour in new NeighbourProvider().GetNeighbours(capital.ToString()))
-                {
-                    var provinceHistory = new ProvinceHistory(neighbour);
-                    provinceHistories.Add(provinceHistory);
-                    // TODO should be from model not from this loop
-                    var firstPrediction = predictor.Predict(provinceHistory).First();
-                    
-                    var provinceViewModel = new ProvinceViewModel
-                        {
-                            ProvinceName = neighbour, 
-                            FarmsViewModel = new FarmsViewModel {FarmsCount = 1}, 
-                            SoldiersViewModel = new FarmsViewModel{FarmsCount = 1},
-                            CultureViewModel = new CultureViewModel(),
-                            BuildPrediction = new BuildPredictionViewModel
-                                {
-                                    Building = firstPrediction.Building, TurnsLeft = firstPrediction.TurnsLeft
-                                },
-                        };
-                    provinceViewModel.ProvinceRemoved += provinceViewModel_OnProvinceRemoved;
-            
-                    viewModel.DatabaseObjects.Add(new MyTemporaryObject
-                        {
-                            ProvinceViewModel = provinceViewModel
-                        });
-                }
-            }
-        }
-
-        void provinceViewModel_OnProvinceRemoved(ProvinceViewModel removedProvince)
-        {
-            for (int i = 0; i < viewModel.DatabaseObjects.Count; i++)
-            {
-                if (viewModel.DatabaseObjects[i].ProvinceViewModel == removedProvince)
-                {
-                    viewModel.DatabaseObjects.RemoveAt(i);
-                }
-            }
-        }
-
-        private void NextTurnClick(object sender, RoutedEventArgs e)
-        {
-            viewModel.Turn++;
-
-            foreach (var provinceViewModel in this.viewModel.DatabaseObjects.Select(p => p.ProvinceViewModel))
-            {
-                var provinceHistory = provinceHistories.Single(p => p.ProvinceName == provinceViewModel.ProvinceName);
-
-                provinceHistory.Add(
-                    new ProvinceRevision(
-                        provinceViewModel.FarmsViewModel.FarmsCount, 
-                        provinceViewModel.SoldiersViewModel.FarmsCount, 
-                        provinceViewModel.CultureViewModel.CultureLevel));
-
-                var predictor = new Predictor();
-                var buildPredictions = predictor.Predict(provinceHistory);
-
-                var firstPrediction = buildPredictions.First();
-                provinceViewModel.BuildPrediction.Building = firstPrediction.Building;
-                provinceViewModel.BuildPrediction.TurnsLeft = firstPrediction.TurnsLeft;
-            }
-        }
-
-        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
-        {
-//            cmbCapital.SelectedIndex = 0;
-            SetUp();
         }
     }
 }
--- src/ProvinceSpy/ViewModels/MainWindowsViewModel.cs
+++ src/ProvinceSpy/ViewModels/MainWindowsViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 
 namespace ProvinceSpy.ViewModels
 {
     public class MainWindowsViewModel : ViewModelBase
     {
+        private readonly List<ProvinceHistory> provinceHistories = new List<ProvinceHistory>();
+
         public IEnumerable<string> Countries { get; private set; }
 
         private string selectedCapital;
@@ -36,12 +39,116 @@ namespace ProvinceSpy.ViewModels
             get { return SeasonsByTurn[Turn % 4]; }
         }
 
+        RelayCommand nextTurnCommand;
+        public ICommand NextTurnCommand
+        {
+            get
+            {
+                if (nextTurnCommand == null)
+                {
+                    nextTurnCommand = new RelayCommand(param => this.OnNextTurn(),
+                        param => true);
+                }
+                return nextTurnCommand;
+            }
+        }
+
+        RelayCommand changeCapitalCommand;
+        public ICommand ChangeCapitalCommand
+        {
+            get
+            {
+                if (changeCapitalCommand == null)
+                {
+                    changeCapitalCommand = new RelayCommand(param => this.OnChangeCapital(),
+                        param => true);
+                }
+                return changeCapitalCommand;
+            }
+        }
+
         public MainWindowsViewModel()
         {
             DatabaseObjects = new ObservableCollection<MyTemporaryObject>();
 
             Countries = new NeighbourProvider().GetCapitals();
             SelectedCapital = Countries.FirstOrDefault();
+
+            OnChangeCapital();
+        }
+
+        private void OnChangeCapital()
+        {
+            Turn = 1;
+
+            DatabaseObjects.Clear();
+
+            provinceHistories.Clear();
+
+            if (SelectedCapital != null)
+            {
+                var predictor = new Predictor();
+                foreach (var neighbour in new NeighbourProvider().GetNeighbours(SelectedCapital))
+                {
+                    var provinceHistory = new ProvinceHistory(neighbour);
+                    provinceHistories.Add(provinceHistory);
+                    // TODO should be from model not from this loop
+                    var firstPrediction = predictor.Predict(provinceHistory).First();
+
+                    var provinceViewModel = new ProvinceViewModel
+                    {
+                        ProvinceName = neighbour,
+                        FarmsViewModel = new FarmsViewModel { FarmsCount = 1 },
+                        SoldiersViewModel = new FarmsViewModel { FarmsCount = 1 },
+                        CultureViewModel = new CultureViewModel(),
+                        BuildPrediction = new BuildPredictionViewModel
+                        {
+                            Building = firstPrediction.Building,
+                            TurnsLeft = firstPrediction.TurnsLeft
+                        },
+                    };
+                    provinceViewModel.ProvinceRemoved += provinceViewModel_OnProvinceRemoved;
+
+                    DatabaseObjects.Add(new MyTemporaryObject
+                    {
+                        ProvinceViewModel = provinceViewModel
+                    });
+                }
+            }
+        }
+
+        private void provinceViewModel_OnProvinceRemoved(ProvinceViewModel removedProvince)
+        {
+            for (int i = 0; i < DatabaseObjects.Count; i++)
+            {
+                if (DatabaseObjects[i].ProvinceViewModel == removedProvince)
+                {
+                    DatabaseObjects.RemoveAt(i);
+                }
+            }
+        }
+
+        private void OnNextTurn()
+        {
+            Turn++;
+
+            foreach (var provinceViewModel in this.DatabaseObjects.Select(p => p.ProvinceViewModel))
+            {
+                var provinceHistory = provinceHistories.Single(p => p.ProvinceName == provinceViewModel.ProvinceName);
+
+                provinceHistory.Add(
+                    new ProvinceRevision(
+                        provinceViewModel.FarmsViewModel.FarmsCount,
+                        provinceViewModel.SoldiersViewModel.FarmsCount,
+                        provinceViewModel.CultureViewModel.CultureLevel));
+
+                var predictor = new Predictor();
+                var buildPredictions = predictor.Predict(provinceHistory);
+
+                var firstPrediction = buildPredictions.First();
+                provinceViewModel.BuildPrediction.Building = firstPrediction.Building;
+                provinceViewModel.BuildPrediction.TurnsLeft = firstPrediction.TurnsLeft;
+            }
         }
     }
 
